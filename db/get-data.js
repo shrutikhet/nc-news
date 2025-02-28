dc0f041ccabe292d7a6457384747e9297ecf0903
shrutikhetan0801 <shrutikhetan0801@gmail.com>
2025-02-28 16:28:42 +0000

    [Committing for task 4]

---
+++ db/get-data.js
@@ -0,0 +1,32 @@
+const db = require("./connection.js");
+const inquirer = require("inquirer");
+
+const {
+  topicData,
+  userData,
+  articleData,
+  commentData,
+} = require("../db/data/development-data/index");
+
+const seed = require("./seeds/seed.js");
+
+const questions = [
+  {
+    name: "tableName",
+    message: "Which table data do you want to see?",
+    default: "topics",
+  },
+];
+
+getData = (tableName) => {
+  return db.query(`select * from ${tableName};`).then((data) => {
+    console.log(data.rows);
+  });
+};
+seed({ topicData, userData, articleData, commentData })
+  .then(() => {
+    return inquirer.prompt(questions);
+  })
+  .then((answers) => {
+    getData(answers.tableName);
+  });
